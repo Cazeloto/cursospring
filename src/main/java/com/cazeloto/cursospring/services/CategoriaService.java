837a6f5ac1a01fab46724261dcba9171df29dcb0
Gerson Cazeloto <gerson@cazeloto.com>
2022-05-06 21:53:27 -0300

    Criando operação Isntanciacao

--- src/main/java/com/cazeloto/cursospring/services/CategoriaService.java
+++ src/main/java/com/cazeloto/cursospring/services/CategoriaService.java
@@ -3,13 +3,14 @@ package com.cazeloto.cursospring.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 import com.cazeloto.cursospring.domain.Categoria;
 import com.cazeloto.cursospring.repositories.CategoriaRepository;
 
 @Service
-public class CategoriaService {
+public class CategoriaService implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository repo;
@@ -17,5 +18,11 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElse(null);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 }
